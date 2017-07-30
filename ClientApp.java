ea804ca9c80390716207e939d4dce3205f08ba7b
jossypapa@gmail.com <jossypapa@gmail.com>
2017-07-30 18:08:40 +0200

    Ds1 project start up

--- ClientApp.java
+++ ClientApp.java
@@ -3,19 +3,20 @@
  * The purpose of this class is t0 handle all the jobs that ClientApp must do
  */
 import java.io.Serializable;
-import akka.actor.ActorRef;
-import akka.actor.ActorSystem;
-import akka.actor.UntypedActor;
+
+import akka.Version;
+import akka.actor.*;
+import akka.pattern.Patterns;
+import akka.util.Timeout;
 import com.typesafe.config.ConfigFactory;
 import com.typesafe.config.Config;
+
+import java.util.*;
 import java.util.concurrent.TimeUnit;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Collections;
-import akka.actor.Props;
-import akka.actor.Cancellable;
+import java.util.regex.Pattern;
+
+import scala.concurrent.Await;
+import scala.concurrent.Future;
 import scala.concurrent.duration.Duration;
 
 public class ClientApp {
@@ -25,21 +26,33 @@ public class ClientApp {
     static private String ip = null;
     static private String port = null;
     static private String value = null;
-    static private String key_value = null;
-    //static private key = null;
-    public static abstract class ClientUser extends UntypedActor {
-        public void preStart() {
-            if (remotePath !=null){
-                switch (usrinput){
-                    case "leave":
-                }
-            }
+    static private String key= null;
+    static private String version = null;
+    public static final int clientTimeout_seconds = 15;
+
+    public static class client extends UntypedActor{
+        public void onReceive(Object message){
+            //if (remotePath !=null){
+                //switch (usrinput){
+                   // case "args[3]": //Leave request
+
+                      //  break;
+                    //case "args[4]": //read request
+
+
+               // }
+            //}
+
+
         }
 
     }
 
 
-//to validate Ip address
+
+
+    //this class is to request read
+    //to validate Ip address
 
         public static boolean validateIPAddress(String ipAddress) {
             String[] input = ipAddress.split("\\.");
@@ -54,68 +67,113 @@ public class ClientApp {
             }
             return true;
         }
-        //To validate port number // <summary>
-        //Port number is an unsigned short from 1-65535,
-        // ports under 1024 are reserved for system services http, ftp, etc.
-        /// Checks if the given string is a valid port number.
 
-      //  public static boolean ValidportNum(String PortNum) {
-         //   String[] input = PortNum;
-       //     if (input.length != 1) {
-         //       return false;
-            //}
-           //for (String str : input) {
-              //  int i = Integer.parseInt(str);
-                //if ((i < 0) || (i > 65536)) {
-                  //  return false;
-                //}
-            //}
-            //return true;
-        //}
+        //According to RFC 793, the port is a 16 bit unsigned int.
+           //This means the range is 0 - 65535( 2^16 - 1)
+          // ports under 1024 are reserved for system services http, ftp, etc.
 
-public static void main (String[] args) {
+    public static boolean ValidportNum (String PortNum) {
+        try {
+            //Port number validation (16bit data)
+            if ( (Integer.parseInt(PortNum) < 0) || (Integer.parseInt(PortNum) > 65535)) {
+                return false;
+            }
+            return true;
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+    }
+
+public static void main (String[] args) throws Exception {
     //usrinput = args[2]
-    if (args.length < 3 && args.length > 5) {
+    if (args.length < 3 || args.length > 5) {
         System.out.println("Invalid user input length : Please,use the the input with length 3-5 ");
         return;
     }
-    //System.out.println("Welcome to user screen");
+    System.out.println("Welcome to user screen");
     if (!validateIPAddress(args[0])) {
         System.out.println("The Ip address is invalid");
         return;
     }
-    //if (!ValidportNum(args[1])) {
-      //  System.out.println("The port number is invalid");
-      //  return;
-   // }
+    if (!ValidportNum(args[1])) {
+       System.out.println("The port number is invalid");
+       return;
+    }
     Config config = ConfigFactory.load("application");
     System.out.println("start loading configuration");
     ip = args[0];
     port = args[1];
-    usrinput = args[2];
+    //usrinput = args[2];   // argument 2 is the user input either read,write or leave
     if (args.length == 3 && args[2].equals("leave")) {              //leave request
-        //ValidportNum(args[1]);
-        remotePath = "akka.tcp://mysystem@" + ip + ":" + port + "/user/node";
-    } else if (args.length == 4 && args[2].equals("read")) {           //read
-        key_value = args[3];
-        remotePath = "akka.tcp://mysystem@" + ip + ":" + port + "/user/node";
-        // The Akka path to the bootstrapping peer
-        remotePath = "akka.tcp://mysystem@" + ip + ":" + port + "/user/node";
-    } else if (args.length == 5 && args[2].equals("write")) {          //write
-        // System.out.println("write ");
-        key_value = args[3];
-        value = args[4];
         remotePath = "akka.tcp://mysystem@" + ip + ":" + port + "/user/node";
 
+
+        System.out.println("The node is which left the ring is" + ip + ":" + port);
+    }
+
+
+
+
+
+    else if (args.length == 4 && args[2].equals("read")) {           //read request
+        key = args[3];
+       // value = args[4];
+        // The Akka path to the bootstrapping peer
+        remotePath = "akka.tcp://mysystem@" + ip + ":" + port + "/user/node";
+
+        // convert the key from string to int
+        int key1 = Integer.parseInt(key);
+
+        // ask to the client
+        ActorSystem system = ActorSystem.create("mysystem", config);
+        ActorSelection myTargetNode = system.actorSelection(remotePath);
+
+        // send the real request
+        Timeout timeout = new Timeout(10, TimeUnit.SECONDS);
+        Future<Object> request = Patterns.ask(myTargetNode, new NodeApp.ClientReadRequest(key1), timeout);
+
+        // we wait for a reply
+        Object reply = Await.result(request, timeout.duration());
+        NodeApp.ClientReadAnswer answer = (NodeApp.ClientReadAnswer) reply;
+
+        // read the answer
+        int value = answer.getValue();
+        System.out.println("The Read value is " + value);
+            //This one is to eliminator the waiting for forever
+        system.shutdown();
+
+        // random string
+        String randomID = UUID.randomUUID().toString();
+
+    }
+        else if (args.length == 5 && args[2].equals("write")) {          //write request
+        // System.out.println("write ");
+        key = args[3];
+        value  = args[4];
+        //version= args[5];
+        remotePath = "akka.tcp://mysystem@" + ip + ":" + port + "/user/node";
+   //Converting from string to int
+        int key2 = Integer.parseInt(key);
+        //int version = Integer.parseInt(version);
+
         // Create the actor system
-        final ActorSystem system = ActorSystem.create("mysystem", config);
-        // Creating single user to communicate with NodeApp
-        final ActorRef ClientUser = system.actorOf(
-                Props.create(ClientUser.class),    // actor class
-                "client");                        // actor name);
+        ActorSystem system = ActorSystem.create("mysystem", config);
+        // Creating single user to communicate with NodeAp
+        ActorSelection myTargetNode = system.actorSelection(remotePath);
+        Timeout timeout = new Timeout(15, TimeUnit.SECONDS);
+        //Using Future’ will send a message to the receiving Actor asynchronously and will immediately return a ‘Future’.
+        Future<Object> request = Patterns.ask(myTargetNode, new NodeApp.ClientWriteRequest(key2,version), timeout);
+        // we wait for a write reply
+        Object reply = Await.result(request, timeout.duration());
+        NodeApp.ClientWriteAnswer answer = (NodeApp.ClientWriteAnswer) reply;
+        // Write the answer
+        int value = answer.getValue();
+        System.out.println("The write value and version is  " + value + version);
+        //This one is to eliminator the waiting for forever
+        system.shutdown();
     }
     else
-        System.out.println("Wrong number of arguments: [remote_ip remote_port 'read' or 'write' or 'leave' key value]");
+        System.out.println("Wrong number of arguments: [remote_ip remote_port 'read' or 'write' or 'leave' or key value]");
 
 }
 }
--- NodeApp.java
+++ NodeApp.java
@@ -7,10 +7,14 @@
  * Distributed Key-Value Store with Data Partitioning and Replication   *
  ************************************************************************
  */
+import java.beans.EventHandler;
 import java.io.Serializable;
+import java.util.EventListener;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Collections;
+
+import akka.Version;
 import akka.actor.ActorRef;
 import akka.actor.UntypedActor;
 import akka.actor.ActorSystem;
@@ -19,15 +23,17 @@ import akka.actor.Cancellable;
 import scala.concurrent.duration.Duration;
 import com.typesafe.config.ConfigFactory;
 import com.typesafe.config.Config;
-
+//The Node class we do the tasks of "Join, Leave, Join write, Join read ,recovery...etc for node" and
+//For client Read,write,join,leave,read version,locale storage,
 public class NodeApp {
     static private String remotePath = null; // Akka path of the bootstrapping peer
     static private int myId; // ID of the local node
-    //static private String command = null;
-    static private int N_sysparametres = 5; //N is a system parameter that defines the degree of replication
-    static private int R_read = 3; //Number of Read Quorum
-    static private int W_write = 3; //Number of Write Quorum
-    static private int T_timer = 600; //Timeout duration
+    static private String value = null;
+    public static  String nodmsg = null;
+    public static final int N_sysparametres = 3; //N is a system parameter that defines the degree of replication
+    public static final int R_read = 2; //Number of Read Quorum
+    public static final int W_write = 2; //Number of Write Quorum
+    static private int T_timer = 10; //Timeout duration in second for internal nodes
     //static private int msTimeoutJoin = 1000;
     //static private int msDataRequest = 5000;
     //static private int msTimeoutRead = T;
@@ -39,13 +45,135 @@ public class NodeApp {
             this.id = id;
         }
     }
-    public static class RequestNodelist implements Serializable {}
+    public static class RequestNodelist implements Serializable {
+
+    }
     public static class Nodelist implements Serializable {
         Map<Integer, ActorRef> nodes;
         public Nodelist(Map<Integer, ActorRef> nodes) {
             this.nodes = Collections.unmodifiableMap(new HashMap<Integer, ActorRef>(nodes));
         }
     }
+    //Read request for replicated nodes
+    public static class Nodereadrequest implements Serializable{
+        String ip;
+        String  port;
+        int nodeId;
+        int version;
+
+        public Nodereadrequest(String ip, String port, int nodeId, int version) {
+            this.ip = ip;
+            this.port = port;
+            this.nodeId = nodeId;
+            this.version = version;
+        }
+    }
+    //Read answer for coordinator for the read request
+    public static class NodereadAnswer implements Serializable{
+        String ip;
+        String port;
+        int nodeId;
+        int version;
+
+        public NodereadAnswer(String ip, String port, int nodeId, int version) {
+            this.ip = ip;
+            this.port = port;
+            this.nodeId = nodeId;
+            this.version = version;
+        }
+    }
+    public static class Nodewriterequest implements Serializable{
+        String value;
+        int key;
+        int version;
+
+        public Nodewriterequest(String value, int key, int version) {
+            this.value = value;
+            this.key = key;
+            this.version = version;
+        }
+    }
+    public static class Leave implements Serializable{
+        int id;
+        public Leave(int id) {  //in this point we can use also port number since it is identical
+            this.id = id;
+    }
+    }
+
+    // client read request  -> a random node
+    public static class ClientReadRequest implements Serializable{
+        private int key;
+
+        public int getKey() {
+            return key;
+        }
+
+        public ClientReadRequest(int key) {
+            this.key = key;
+        }
+    }
+
+    public static class ClientReadAnswer implements Serializable {
+        private int key;
+        private int value;
+
+        public ClientReadAnswer(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public int getKey() {
+            return key;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    //client write request  -> a random node
+public static class ClientWriteRequest implements Serializable{
+    private int key;
+    private String version;
+
+    public int getKey() {
+        return key;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+        public ClientWriteRequest(int key, String version) {
+            this.key = key;
+            this.version = version;
+        }
+    }
+    public static class ClientWriteAnswer implements Serializable{
+        private int key;
+        private int value;
+        private String version;
+
+        public int getKey() {
+            return key;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public String getVersion() {
+            return version;
+        }
+
+        public ClientWriteAnswer(int key, int value, String version) {
+            this.key = key;
+            this.value = value;
+            this.version = version;
+        }
+    }
+
+
     //The Joined guest node requesting information from exiting nodes
     public static class nodesInfo implements Serializable{
         int id;
@@ -59,7 +187,6 @@ public class NodeApp {
     }
 
     public static class Node extends UntypedActor {
-
         // The table of all nodes in the system id->ref
         private Map<Integer, ActorRef> nodes = new HashMap<>();
 
@@ -85,18 +212,34 @@ public class NodeApp {
                 System.out.println("Node " + id + " joined");
                 nodes.put(id, getSender());
             }
+
+            // read operation from the client
+            else if(message instanceof ClientReadRequest) {
+                ClientReadRequest request =(ClientReadRequest) message;
+                // TODO> implement protocol
+                //int value1 = Integer.parseInt(value);
+                getSender().tell(new ClientReadAnswer(request.getKey(), 12), getSelf());
+            }
+            //write operation from client
+            else if (message instanceof ClientWriteRequest) {
+                ClientWriteRequest request = (ClientWriteRequest) message;
+                getSender().tell(new ClientWriteAnswer(request.getKey(), 13,request.getVersion()), getSelf());
+            }
+            else if (message instanceof Leave){
+                System.out.println("The Node have to share data before leaving");
+
+            }
             else
                 unhandled(message);		// this actor does not handle any incoming messages
         }
     }
 
     public static void main(String[] args) {
-
-        if (args.length != 0 && args.length !=2 ) {
-            System.out.println("Wrong number of arguments: [remote_ip remote_port]");
+                    //the will check node IP and Port
+        if (args.length == 1|| args.length > 3) {
+            System.out.println("Please use the argument: [remote_ip remote_port]");
             return;
         }
-
 
         // Load the "application.conf"
         Config config = ConfigFactory.load("application");
@@ -109,12 +252,34 @@ public class NodeApp {
             remotePath = "akka.tcp://mysystem@"+ip+":"+port+"/user/node";
             System.out.println("Starting node " + myId + "; bootstrapping node: " + ip + ":"+ port);
         }
+        else if (args.length == 3) {
+            if (args[0].equals("join")) {
+                nodmsg = "join";
+                String ip = args[1];
+                String port = args[2];
+                // The Akka path to the bootstrapping peer
+                remotePath = "akka.tcp://mysystem@" + ip + ":" + port + "/user/node";
+                System.out.println("The Joining Node is  " + ip + ":" + port);
+            } else if (args[0].equals("recover")) { //here the recovery will be occurred
+                nodmsg = "recover";
+                String ip = args[1];
+                String port = args[2];
+                // The Akka path to the bootstrapping peer
+                remotePath = "akka.tcp://mysystem@" + ip + ":" + port + "/user/node";
+                System.out.println("Successful Recovery for Node  " + ip + ":" + port);
+
+            }
+            else System.out.println("Invalid data: Please check your input data [spelling,ip or port]");
+        }
+
         else
             System.out.println("Starting disconnected node " + myId);
 
-       // if(myId<0||myId>10000){																	//10000 is Random number to add this feature
+       // if(myId<0||myId>10000){				//10000 is Random number to add this feature
            // System.out.println("Invalid node id. Please, use in between 0 and 10000");
            // return;
+
+
 
 
        // N = 5;
@@ -141,7 +306,6 @@ public class NodeApp {
 
 
         // Create the actor system
-        //final ActorSystem system = ActorSystem.create("mysystem", config);
         final ActorSystem system = ActorSystem.create("mysystem", config);
 
 
